121d5846d17c670f65d459f7774f6ddd778b2fb2
229194-21 <229194-21>
2021-03-31 11:22:25 +0300

    now its a PROJECT

---
+++ zadanie7/zadanie7/Source.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+using namespace std;
+
+void massive(int matrix[][10], int m)
+{
+	int i, j;
+	for (i = 0; i < m; i++)
+	{
+		cout << "\n";
+		for (j = 0; j < m; j++)
+		{
+			cout << matrix[i][j] << "     ";
+		}
+	}
+	cout << "\n";
+}
+
+int main()
+{
+	int i, j;
+	int matrix[10][10];
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			matrix[i][j] = rand() % 100;
+		}
+	}
+	cout << "matrix: " << endl;
+	massive(matrix, 10);
+	return 0;
+
+}
+system("PAUSE");
